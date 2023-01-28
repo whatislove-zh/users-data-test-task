c9cc7fa0c5353d5ea14cde9460adae92828f01db
Vladislav <vladgm233223@gmail.com>
2023-01-29 00:44:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header";
 import { Container } from "@mui/system";
 import { Hero } from "./components/Hero";
+import { Users } from "./components/Users";
+import { SignUp } from "./components/SignUp";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Container disableGutters maxWidth="lg">
         <Header />
         <Hero />
+        <Users />
+        <SignUp />
       </Container>
     </>
   );
