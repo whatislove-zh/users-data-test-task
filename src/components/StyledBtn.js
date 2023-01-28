c1be8f1b0973a2a90117f7044ab3aabb9d585034
Vladislav <vladgm233223@gmail.com>
2023-01-28 17:56:18 +0200

    Create StyledBtn.js

---
+++ src/components/StyledBtn.js
@@ -0,0 +1,24 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const CustomButton = styled(Button)({
+  background: "#F4E041",
+  border: "none",
+  borderRadius: "80px",
+  color: "rgba(0, 0, 0, 0.87)",
+  width: "100px",
+  height: "34px",
+  marginLeft: "10px",
+  "&:hover": {
+    background: "#FFE302",
+  },
+  "&.Mui-disabled": {
+    background: "#B4B4B4",
+    color: "#ffffff",
+  },
+});
+
+export const StyledBtn = (props) => {
+  const { title, disabled = false } = props;
+  return <CustomButton disabled={disabled}>{title}</CustomButton>;
+};
