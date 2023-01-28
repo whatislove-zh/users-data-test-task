70b894b864fc9b11e4e706850caa3f3a81010857
Vladislav <vladgm233223@gmail.com>
2023-01-28 17:56:02 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { AppBar, Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import logo from "../assets/Logo.svg";
+import { StyledBtn } from "./StyledBtn";
+
+import React from "react";
+
+export const Header = () => {
+  return (
+    <Box>
+      <AppBar position="static" sx={{ background: "none" }}>
+        <Toolbar
+          sx={{ display: "flex", justifyContent: "space-between", mx: "60px" }}
+        >
+          <Box
+            component="img"
+            alt="logo"
+            src={logo}
+            height="26px"
+            width="104px"
+          />
+          <Box sx={{ my: "13px" }}>
+            <StyledBtn title="Users" />
+            <StyledBtn title="Sign up" />
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
