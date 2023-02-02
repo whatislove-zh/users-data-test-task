1f437020b950ddeef288cb01394a80cee4a741f8
Vladislav <vladgm233223@gmail.com>
2023-02-02 11:59:41 +0200

    get users from firedase

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,30 +1,49 @@
 import { Grid, Typography, Box } from "@mui/material";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import data from "../data.js";
+import { db } from "../firebase.js";
+import { setExpand } from "../store/expanded/expanded-actions.js";
+import { selectExpand } from "../store/expanded/expanded-selector.js";
 import { StyledBtn } from "./StyledBtn";
 import { UserCard } from "./UserCard";
 
-const DATA = data();
 
 export const Users = () => {
   const [users, setUsers] = useState([]);
-  const [numberOfitemsShown, setNumberOfItemsToShown] = useState(3);
-  const [expanded, setExpanded] = useState(false);
 
+  const dispatch = useDispatch();
+  
+  const expanded = useSelector(selectExpand)
+  
+  const [numberOfitemsShown, setNumberOfItemsToShown] = useState(6);
+  
   const showMore = () => {
     if (numberOfitemsShown + 3 <= users.length) {
       setNumberOfItemsToShown(numberOfitemsShown + 3);
     } else {
       setNumberOfItemsToShown(users.length);
-      setExpanded(true);
+      dispatch(setExpand(true));
     }
   };
 
-  useEffect(() => {
-    setUsers(DATA);
-  }, []);
+  const getUsers = async () => {
+    const collectionRef = collection(db, "users")
+    const queryColection = query(collectionRef, orderBy("timestamp", "desc"))
+    const unsubscribe = onSnapshot(queryColection, (snapshot) => {
+      setUsers(snapshot.docs.map((doc) => ({...doc.data()})))
+    })
+    return unsubscribe
+  }
 
+  
+
+  useEffect(() => {
+    getUsers();
+    dispatch(setExpand(false))
+  }, [dispatch]);
+  
   return (
     <Box sx={{ mb: "140px" }} align="center">
       <Typography
@@ -38,7 +57,7 @@ export const Users = () => {
       </Typography>
       <Grid container spacing="29" align="center" sx={{ mb: "50px" }}>
         {users.slice(0, numberOfitemsShown).map((user) => (
-          <Grid key={user.name} item lg={4} sm={6} xs={12}>
+          <Grid key={user.userId} item lg={4} sm={6} xs={12}>
             <UserCard user={user} />
           </Grid>
         ))}
